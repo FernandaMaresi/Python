3aab4c4ee68e4b1e1b585ad0c4f2075b449c919d
Fernanda Maresi <ferferborges@gmail.com>
2021-03-06 12:20:07 -0300

    Add files via upload

---
+++ ex001.py
@@ -0,0 +1,36 @@
+print('======= Tabuada =======')
+n = int(input('Digite um numero: '))
+num1 = 1
+num2 = 2
+num3 = 3
+num4 = 4
+num5 = 5
+num6 = 6
+num7 = 7
+num8 = 8
+num9 = 9
+num10 = 10
+n1 = n* num1
+n2 = n* num2
+n3 = n* num3
+n4 = n* num4
+n5 = n* num5
+n6 = n* num6
+n7 = n* num7
+n8 = n* num8
+n9 = n* num9
+n10 = n* num10
+
+print('{} x {} = {}'.format(n,num1,n1))
+print('{} x {} = {}'.format(n,num2,n2))
+print('{} x {} = {}'.format(n,num3,n3))
+print('{} x {} = {}'.format(n,num4,n4))
+print('{} x {} = {}'.format(n,num5,n5))
+print('{} x {} = {}'.format(n,num6,n6))
+print('{} x {} = {}'.format(n,num7,n7))
+print('{} x {} = {}'.format(n,num8,n8))
+print('{} x {} = {}'.format(n,num9,n9))
+print('{} x {} = {}'.format(n,num10,n10))
+print('=========================')
+
+
