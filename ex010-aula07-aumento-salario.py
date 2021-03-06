30a180bbf1bbbfcc7209e183efcd119b6514c3da
Fernanda Maresi <ferferborges@gmail.com>
2021-03-06 12:19:31 -0300

    Add files via upload

---
+++ ex010-aula07-aumento-salario.py
@@ -0,0 +1,9 @@
+#-*-coding:utf8;-*-
+#qpy:console
+
+print('================================')
+salario = int(input('Salario: '))
+novoSalario = salario + (salario * (15/100))
+print( ' ====== Aumento de 15% =======')
+print(' O novo salario sera {} '.format(novoSalario))
+print('=============================')
